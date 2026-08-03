08da58dc53ac98c66ef51c5c133476bbe76f1705
anishelf <team.anishelf@gmail.com>
2026-08-03 02:40:29 +0200

    Fix cookie JWT middleware

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -3,23 +3,10 @@ const jwt = require("jsonwebtoken");
 
 function verifyToken(req,res,next){
 
-    const authHeader =
-    req.headers.authorization;
 
-
-    if(!authHeader){
-
-        return res.status(401).json({
-
-            success:false,
-
-            message:"No token provided"
-
-        });
-
-    }
     const token =
     req.cookies.token;
+
 
 
     if(!token){
@@ -34,7 +21,41 @@ function verifyToken(req,res,next){
 
     }
 
-   
+
+
+    try{
+
+
+        const decoded =
+        jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        );
+
+
+        req.user =
+        decoded;
+
+
+
+        next();
+
+
+
+    }catch(error){
+
+
+        return res.status(401).json({
+
+            success:false,
+
+            message:"Invalid token"
+
+        });
+
+
+    }
+
 
 }
 
